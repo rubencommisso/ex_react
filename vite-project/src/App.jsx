37fde3840ae037fca166f820f04db3dd0dd04cf2
rubencommisso <commissoruben@gmail.com>
2025-02-19 12:36:46 +0100

    add class component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+class app extends Component {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  title = "Titolo 1";
+
+  render () {
+    return (
+      <>
+        <p>Hello world!</p>
+        <h1>{this.title}</h1>
+      </>
+    )
+  }
 }
 
-export default App
+export default app;
