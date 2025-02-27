b84e662c98946b4fb7d1f39501fd44a20e7b256d
rubencommisso <commissoruben@gmail.com>
2025-02-27 20:11:18 +0100

    add state and button that encrease number

--- vite-project/src/Counter.jsx
+++ vite-project/src/Counter.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react"
+
 const Counter = ()=> {
-    return (<div>count:0</div> )
+    const [number,setNumber]= useState(0);
+
+    const clickOnButton = () => {
+        setNumber((_counter) => {
+            return _counter +1;
+        });
+    }
+
+    return (
+        <div>
+            <button onClick={clickOnButton}>Add</button>
+            <p>Counter: {number}</p>
+            
+        </div> )
 }
 
 export default Counter 
