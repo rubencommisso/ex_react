d0140879aa5416d8b8df19efd50aabbd24fe9309
rubencommisso <commissoruben@gmail.com>
2025-02-27 20:13:53 +0100

    add button to cut number

--- vite-project/src/Counter.jsx
+++ vite-project/src/Counter.jsx
@@ -3,15 +3,23 @@ import { useState } from "react"
 const Counter = ()=> {
     const [number,setNumber]= useState(0);
 
-    const clickOnButton = () => {
+    const clickOnButtonAdd = () => {
         setNumber((_counter) => {
             return _counter +1;
         });
     }
 
+    const clickOnButtonCut = () => {
+        setNumber((_counter) => {
+            return _counter -1;
+        });
+    }
+
     return (
         <div>
-            <button onClick={clickOnButton}>Add</button>
+            <button onClick={clickOnButtonAdd}>Add</button>
+            <button onClick={clickOnButtonCut}>cut</button>
+
             <p>Counter: {number}</p>
             
         </div> )
