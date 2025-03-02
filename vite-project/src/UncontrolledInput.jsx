ae5752983a792a3cf4801be5cf41f44bbfb3c15b
rubencommisso <commissoruben@gmail.com>
2025-03-02 19:40:19 +0100

    add button

--- vite-project/src/UncontrolledInput.jsx
+++ vite-project/src/UncontrolledInput.jsx
@@ -3,12 +3,14 @@ import { useRef } from "react";
 const UncontrolledInput = () => {
     const inputRef = useRef(null); 
 
-    
+    const handleClick = () => {
+        alert(`Valore dell'input: ${inputRef.current.value}`); 
+    };
 
     return (
         <div>
             <input type="text" ref={inputRef} /> {}
-            
+            <button onClick={handleClick}>Mostra Valore</button>
         </div>
     );
 };
