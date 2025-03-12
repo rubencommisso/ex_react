d86c423c2b3230c16d0d2daf47eb8d9698eace10
rubencommisso <commissoruben@gmail.com>
2025-03-12 09:46:35 +0100

    add react router and routes

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import TodoList from "../TodoList.jsx";
+
+const Home = () => {
+    return(
+        <>
+        <h1>Home</h1>
+        <TodoList/>
+        </>
+        
+
+    )
+}
+
+export default Home
